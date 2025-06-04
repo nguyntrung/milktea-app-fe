33fd69dbd7a3aa874ab60fb28c9a79920ce610fd
nguyntrung <nguyntrung203@gmail.com>
2025-06-04 16:39:59 +0700

    baseURL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 // Tạo instance
 const api = axios.create({
-  // baseURL: 'https://trasua.up.railway.app',
-  baseURL: 'http://localhost:5000',
-  timeout: 20000,
+  baseURL: 'https://trasua.up.railway.app',
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
